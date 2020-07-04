4a4a997ad2566e19d3d33127c8e0caab0d00b00f
Helena1981 <gostep@ya.ru>
2020-07-04 20:28:37 +0300

    добавили popup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log('Привет!', '2 + 3 = ', 2+3)
+
+ const openPopupButton = document.querySelector('.kusto__pen');
+ const closePopupButton = document.querySelector('.popup__close');
+ const popup = document.querySelector('.popup');
+ 
+ function togglePopup() {
+     popup.classList.toggle('popup_opened');
+ }
+
+ openPopupButton.addEventListener('click', togglePopup);
+ closePopupButton.addEventListener('click', togglePopup);
+ 
+ 
+//  console.log(closePopupButton);
