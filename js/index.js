f8bde78a15b093238248020f540312b788db8877
Helena1981 <gostep@ya.ru>
2020-07-04 23:05:28 +0300

    save

--- js/index.js
+++ js/index.js
@@ -11,4 +11,39 @@
  closePopupButton.addEventListener('click', togglePopup);
  
  
-//  console.log(closePopupButton);
+
+let pageName = document.querySelector('.kusto__title').textContent;
+let pagejob = document.querySelector('.kusto__subtitle').textContent;
+
+
+
+
+console.log(pagejob);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Подтягиваем имя и работу со страницы. Простая конструкция
+// var pageName = document.querySelector('.kusto__title').textContent;
+// var inputName = document.getElementById('input-name');
+// inputName.setAttribute('value', pageName);
+
+// Подтягиваем имя и работу со страницы. Одна строчка
+
+// document.getElementById('input-name').setAttribute('value', document.querySelector('.kusto__title').textContent);
+
