ee17b66606e42c34ea3092d65a11424f2aa3c32e
Helena1981 <gostep@ya.ru>
2020-07-07 00:40:48 +0300

    save

--- js/index.js
+++ js/index.js
@@ -1,37 +1,33 @@
+const openPopupButton = document.querySelector('.kusto__pen');
+const closePopupButton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
 
- const openPopupButton = document.querySelector('.kusto__pen');
- const closePopupButton = document.querySelector('.popup__close');
- const popup = document.querySelector('.popup');
- 
- function togglePopup() {
-     popup.classList.toggle('popup_opened');
- }
-
- openPopupButton.addEventListener('click', togglePopup);
- closePopupButton.addEventListener('click', togglePopup);
- 
- 
 // Берём заголовок и подзаголовок на странице
 let pageName = document.querySelector('.kusto__title').textContent;
 let pagejob = document.querySelector('.kusto__subtitle').textContent;
 
 // Берём текст из полей ввода 
-let inname = document.getElementById('input-name');
-let injob = document.getElementById('input-job');
+let inname = document.querySelectorAll('.form__input')[0];
+let injob = document.querySelectorAll('.form__input')[1];
 
-// Устанавливаем атрибут value 
-inname.setAttribute('value', pageName);
-injob.setAttribute('value', pagejob);
+function togglePopup() {
+    popup.classList.toggle('popup_opened');
+    // Устанавливаем атрибут value для полей ввода
+    inname.setAttribute('value', pageName);
+    injob.setAttribute('value', pagejob);
+}
+
+openPopupButton.addEventListener('click', togglePopup);
+closePopupButton.addEventListener('click', togglePopup);
+
 
 // Записываем введенные значения на страницу
-const saveButton = document.getElementById('savebutt');
+const saveButton = document.querySelector('.form__button');
 
- function saveInf() {
+function saveInf() {
     document.querySelector('.kusto__title').textContent = inname.value;
     document.querySelector('.kusto__subtitle').textContent = injob.value;
     togglePopup();
- }
+}
 
- saveButton.addEventListener('click', saveInf);
-
-
+saveButton.addEventListener('click', saveInf);
