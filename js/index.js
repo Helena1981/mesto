300c512824e9bbdb821e08d2b5a222de8135abec
Helena1981 <gostep@ya.ru>
2020-07-07 21:51:44 +0300

    save
    всё починили

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@ let pageName = document.querySelector('.profile__title').textContent;
 let pagejob = document.querySelector('.profile__subtitle').textContent;
 
 // Берём текст из полей ввода 
-let inname = document.querySelectorAll('.form__input')[0];
-let injob = document.querySelectorAll('.form__input')[1];
+let inname = document.querySelector('.form__input_name');
+let injob = document.querySelector('.form__input_job');
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
@@ -20,14 +20,16 @@ function togglePopup() {
 openPopupButton.addEventListener('click', togglePopup);
 closePopupButton.addEventListener('click', togglePopup);
 
-
 // Записываем введенные значения на страницу
 const saveButton = document.querySelector('.form__button');
 
 function saveInf() {
+    event.preventDefault();
     document.querySelector('.profile__title').textContent = inname.value;
     document.querySelector('.profile__subtitle').textContent = injob.value;
-    togglePopup();
+    
+    popup.classList.toggle('popup_opened');
+    
 }
 
 saveButton.addEventListener('click', saveInf);
