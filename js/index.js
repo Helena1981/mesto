03d9b4080e4fd5bcedf2d3a6310628842809e750
Helena1981 <gostep@ya.ru>
2020-07-21 00:11:44 +0300

    добавлен template для вывод карточки

--- js/index.js
+++ js/index.js
@@ -32,3 +32,69 @@ document.querySelector('.popup__form').addEventListener('submit', (event) => {
     event.preventDefault()
     saveInf();
 });
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+// ---------
+// Работающий код на одну карточку 
+const cardTemplate = document.querySelector('#initItem').content;
+const cardOnline = document.querySelector('.card__items');
+let cardElement = cardTemplate.cloneNode(true);
+
+cardElement.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+cardElement.querySelector('.card__title').textContent = 'Архыз';
+
+cardOnline.append(cardElement);
+// ---------
+
+
+
+// // Работающий код на все карточки
+// const cardTemplate = document.querySelector('#initItem').content; //Берём template
+// const cardOnline = document.querySelector('.card__items'); //Берём элемент, внутрь которого будет вставлять код из template
+
+// function cardRender (lin, nam) { //Объявляем функциюс аргументами
+//     let cardElement = cardTemplate.cloneNode(true); // Создаём переменную и клонируем в неё наш template
+//     cardElement.querySelector('.card__image').src = initialCards[lin].link; //Нужному элементу назначаем ссылку на картинку из массива
+//     cardElement.querySelector('.card__title').textContent = initialCards[nam].name; //Нужному элементу назначаем текстовый контент из массива
+//     cardOnline.append(cardElement); //Выводим склонированный шаблон с подготовленным контентом
+// }
+
+// // cardRender (0, 0);
+// // cardRender (1, 1);
+// // cardRender (2, 2);
+
+
+// initialCards.forEach(function (item, i) { // Запускаем цикл вывода шаблона с новыми параметрами
+//     cardRender (i, i);
+// });
+
+
+
+
+
