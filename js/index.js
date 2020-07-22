1d68102f88954119d4429566196c3959160b2ff5
Helena1981 <gostep@ya.ru>
2020-07-22 23:33:38 +0300

    save

--- js/index.js
+++ js/index.js
@@ -64,15 +64,51 @@ const initialCards = [
 // Работающий код на одну карточку 
 const cardTemplate = document.querySelector('#initItem').content;
 const cardOnline = document.querySelector('.card__items');
-let cardElement = cardTemplate.cloneNode(true);
-
-cardElement.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
-cardElement.querySelector('.card__title').textContent = 'Архыз';
-
-cardOnline.append(cardElement);
-// ---------
 
 
+function cardRender(lin, nam) {
+    let cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__image').src = initialCards[lin].link;
+    cardElement.querySelector('.card__title').textContent = initialCards[nam].name;
+    cardElement.querySelector('.card__image').alt = initialCards[nam].name;
+    cardOnline.append(cardElement);
+}
+
+function addCard() {
+    let cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__image').src = cardLink.value;
+    cardElement.querySelector('.card__title').textContent = cardName.value;
+    cardElement.querySelector('.card__image').alt = cardName.value;
+    cardOnline.prepend(cardElement);
+ 
+}
+ initialCards.forEach(function (item, i) { // Запускаем цикл вывода шаблона с новыми параметрами
+     cardRender (i, i);
+    
+});  
+
+
+
+const openCardButton = document.querySelector('.profile__button');
+const closeCardButton = document.querySelector('.popup__close_card');
+const popupCard = document.querySelector('.popup_card');
+
+let cardName = document.querySelector('.form__card_name');
+let cardLink = document.querySelector('.form__card_link');
+
+function togglepopupCard() {
+    popupCard.classList.toggle('popup_opened');
+    
+}
+
+openCardButton.addEventListener('click', togglepopupCard);
+closeCardButton.addEventListener('click', togglepopupCard);
+
+popupCard.querySelector('.popup__form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    addCard();
+    togglepopupCard();
+});
 
 // // Работающий код на все карточки
 // const cardTemplate = document.querySelector('#initItem').content; //Берём template
